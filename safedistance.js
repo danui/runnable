0f34a6284d1c7a0e76fb4a447e63c722a5abc576
jin <hsienjin@gmail.com>
2019-10-26 20:33:56 +0800

    Compute safe distances 3 days ahead

--- safedistance.js
+++ safedistance.js
@@ -1,39 +1,51 @@
+window.onload = (function () {
+    var MILLISECONDS = 1;
+    var SECONDS = 1000 * MILLISECONDS;
+    var MINUTES = 60 * SECONDS;
+    var HOURS = 60 * MINUTES;
+    var DAYS = 24 * HOURS;
+    var WEEKS = 7 * DAYS;
+    
+    function getParam(key) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	var i;
+	var pair;
+	for (i=0; i < vars.length; ++i) {
+            pair = vars[i].split("=");
+            if (pair[0] == key) {
+		return pair[1];
+	    }
+	}
+	return "";
+    }
 
-function getParam(key) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    var i;
-    var pair;
-    for (i=0; i < vars.length; ++i) {
-        pair = vars[i].split("=");
-        if (pair[0] == key) {
-	    return pair[1];
+    function getSafeDistance(targetDistance, rate, wks) {
+	if (wks <= 0) {
+	    return targetDistance;
+	}
+	return targetDistance / Math.pow(rate, wks);
+    }
+
+    function getWeeks(targetTs, days) {
+	var now = new Date().getTime();
+	var fromTs = now + (days * DAYS);
+	return (targetTs - fromTs) / WEEKS;
+    }
+
+    function main() {
+	var inputDate = getParam("date");
+	var inputKm = getParam("km");
+	var inputRate = getParam("rate");
+	var targetTs = new Date(inputDate).getTime();
+	var i;
+	document.getElementById("target-date").innerHTML = inputDate;
+	document.getElementById("target-distance").innerHTML = inputKm;
+	document.getElementById("target-rate").innerHTML = inputRate;
+	for (i = 0; i < 4; ++i) {
+	    document.getElementById("safe-distance-" + i).innerHTML = getSafeDistance(inputKm, inputRate, getWeeks(targetTs, i)).toFixed(2);
 	}
     }
-    return "";
-}
 
-function getSafeDistance(targetDistance, rate, wks) {
-    if (wks <= 0) {
-	return targetDistance;
-    }
-    return targetDistance / Math.pow(rate, wks);
-}
-
-function safeDistanceMain() {
-    var inputDate = getParam("date");
-    var inputKm = getParam("km");
-    var inputRate = getParam("rate");
-    var t1 = new Date(inputDate).getTime();
-    var t0 = new Date().getTime();
-    var wks = (t1 - t0) / 1000 / 60 / 60 / 24 / 7;
-    var safeDistance = getSafeDistance(inputKm, inputRate, wks);
-    
-    
-    document.getElementById("target-date").innerHTML = inputDate;
-    document.getElementById("target-distance").innerHTML = inputKm;
-    document.getElementById("target-rate").innerHTML = inputRate;
-    document.getElementById("safe-distance").innerHTML = safeDistance.toFixed(2);
-}
-
-window.onload = safeDistanceMain;
+    main();
+});
